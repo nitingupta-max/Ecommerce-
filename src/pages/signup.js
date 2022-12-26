85807ded32fbefb91bd3702db78fae7034e0e95b
nitingupta-max <masternitin1234@gmail.com>
2022-12-26 23:26:12 +0530

    registration

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -5,7 +5,51 @@ import "../css/signup.css";
 import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import play_app_icon from "../image/catalog/app.png";
+import { useState } from "react";
 const Signup = () => {
+  const [data, setData] = useState({
+    typeOfBusiness: "",
+    ownerName: "",
+    businessAddress: "",
+    pincode: "",
+    city: "",
+    area: "",
+    state: "",
+    phoneNumber: "",
+    email: "",
+    password: "",
+    pharmacistPhoneNumber: "",
+    drugsLicenseNumber: "",
+    drugsLicense: "",
+    gstInNumber: "",
+    gstCertificate: "",
+    panNumber: "",
+  });
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  // const [error, setError] = useState("");
+  // const navigate = useNavigate();
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const url = "https://phnebook.heroku.com/api/users";
+  //     const { data: res } = await axios.post(url, data);
+  //     navigate("/login");
+  //     console.log(res.message);
+  //   } catch (error) {
+  //     if (
+  //       error.response &&
+  //       error.response.status >= 400 &&
+  //       error.response.status <= 500
+  //     ) {
+  //       setError(error.response.data.message);
+  //     }
+  //   }
+  // };
+
   return (
     <>
       {/* <Navbar /> */}
@@ -33,6 +77,11 @@ const Signup = () => {
                 <input
                   placeholder="Type of Business"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="typeOfBusiness"
+                  onChange={handleChange}
+                  value={data.typeOfBusiness}
                 ></input>
               </div>
               <div>
@@ -42,13 +91,26 @@ const Signup = () => {
                 <input
                   placeholder="Type of Business"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="typeOfBusiness"
+                  onChange={handleChange}
+                  value={data.typeOfBusiness}
                 ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Owner Name</strong>
                 </Typography>
-                <input placeholder="Owner Name" className="form-data"></input>
+                <input
+                  placeholder="Owner Name"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="ownerName"
+                  onChange={handleChange}
+                  value={data.ownerName}
+                ></input>
               </div>
               <div>
                 <Typography>
@@ -57,57 +119,109 @@ const Signup = () => {
                 <input
                   placeholder="Business Address"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="businessAddress"
+                  onChange={handleChange}
+                  value={data.businessAddress}
                 ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Pincode</strong>
                 </Typography>
-                <input placeholder="Pincode" className="form-data"></input>
+                <input
+                  placeholder="Pincode"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="pincode"
+                  onChange={handleChange}
+                  value={data.pincode}
+                ></input>
               </div>
               <div>
                 <Typography>
                   <strong>City</strong>
                 </Typography>
-                <input placeholder="City" className="form-data"></input>
+                <input
+                  placeholder="City"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="city"
+                  onChange={handleChange}
+                  value={data.city}
+                ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Area</strong>
                 </Typography>
-                <input placeholder="Area" className="form-data"></input>
+                <input
+                  placeholder="Area"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="area"
+                  onChange={handleChange}
+                  value={data.area}
+                ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Select State</strong>
                 </Typography>
-                <input placeholder="Select State" className="form-data"></input>
+                <input
+                  placeholder="Select State"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="state"
+                  onChange={handleChange}
+                  value={data.state}
+                ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Phone Number</strong>
                 </Typography>
-                <input placeholder="Phone Number" className="form-data"></input>
+                <input
+                  placeholder="Phone Number"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="phoneNumber"
+                  onChange={handleChange}
+                  value={data.phoneNumber}
+                ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Email</strong>
                 </Typography>
-                <input placeholder="Password" className="form-data"></input>
+                <input
+                  placeholder="Password"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="password"
+                  onChange={handleChange}
+                  value={data.password}
+                ></input>
               </div>
               <div>
                 <Typography>
                   <strong>Password</strong>
                 </Typography>
-                <input placeholder="Password" className="form-data"></input>
-              </div>
-              <div>
-                <Typography>
-                  <strong>Confirm Password</strong>
-                </Typography>
                 <input
-                  placeholder="Confirm Password"
+                  placeholder="Password"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="password"
+                  onChange={handleChange}
+                  value={data.password}
                 ></input>
               </div>
               <div>
@@ -117,6 +231,21 @@ const Signup = () => {
                 <input
                   placeholder="Confirm Password"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="confirmpassword"
+                ></input>
+              </div>
+              <div>
+                <Typography>
+                  <strong>Confirm Password</strong>
+                </Typography>
+                <input
+                  placeholder="Confirm Password"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="confirmpassword"
                 ></input>
               </div>
               <div>
@@ -126,6 +255,11 @@ const Signup = () => {
                 <input
                   placeholder="Pharmacist Phone No. 1"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="pharmaNUmber"
+                  onChange={handleChange}
+                  value={data.pharmacistPhoneNumber}
                 ></input>
               </div>
               <div>
@@ -135,6 +269,11 @@ const Signup = () => {
                 <input
                   placeholder="Add Drug License number"
                   className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="licenseNumber"
+                  onChange={handleChange}
+                  value={data.drugsLicenseNumber}
                 ></input>
               </div>
               <div>
@@ -146,6 +285,9 @@ const Signup = () => {
                   id="file-upload"
                   name="file-upload"
                   className="form-data"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={data.drugsLicense}
                 />
                 {/* <input placeholder="Password" className="form-data"></input> */}
               </div>
@@ -153,7 +295,15 @@ const Signup = () => {
                 <Typography>
                   <strong>GSTIN Number</strong>
                 </Typography>
-                <input placeholder="GSTIN Number" className="form-data"></input>
+                <input
+                  placeholder="GSTIN Number"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="gstinnumber"
+                  onChange={handleChange}
+                  value={data.gstInNumber}
+                ></input>
               </div>
               <div>
                 <Typography>
@@ -164,13 +314,24 @@ const Signup = () => {
                   id="file-upload"
                   name="file-upload"
                   className="form-data"
+                  autoComplete="off"
+                  onChange={handleChange}
+                  value={data.gstCertificate}
                 />
               </div>
               <div>
                 <Typography>
                   <strong>Pan Number</strong>
                 </Typography>
-                <input placeholder="Pan Number" className="form-data"></input>
+                <input
+                  placeholder="Pan Number"
+                  className="form-data"
+                  type="text"
+                  autoComplete="off"
+                  name="panNumber"
+                  onChange={handleChange}
+                  value={data.panNumber}
+                ></input>
               </div>
             </div>
 
